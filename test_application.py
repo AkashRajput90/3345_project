df3fdb9b496be599f099fad788af7bc2b55695ee
Akash Manhas <122945144+AkashRajput90@users.noreply.github.com>
2023-11-26 13:07:51 -0500

    Create test_application.py

---
+++ test_application.py
@@ -0,0 +1,10 @@
+# test_application.py
+
+from your_application_file import app
+import pytest
+
+# Example test to check if the index route returns a 200 status code
+def test_index_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
